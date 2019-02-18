f0e13f847e61b1c4a6b6df092f420a3e6fd7628b
KangLin <kl222@126.com>
2019-02-18 22:33:44 +0800

    Modify appveyor.yml

--- ci/build.sh
+++ ci/build.sh
@@ -80,7 +80,7 @@ $MAKE -f Makefile
 echo "$MAKE install ...."
 $MAKE install
 
-if [ "${BUILD_TARGERT}" = "windows_msvc" ]; then
+if [ "${BUILD_TARGERT}" != "android" ]; then
     cd ${SOURCE_DIR}
     cp Install/Install.nsi build_${BUILD_TARGERT}
     "/C/Program Files (x86)/NSIS/makensis.exe" "build_${BUILD_TARGERT}/Install.nsi"
