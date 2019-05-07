640b9e37c6b2c27f98f25ffef552e0dfee833c9e
KangLin <kl222@126.com>
2019-05-07 18:41:19 +0800

    Add setSizePolicy

--- Src/FrmCalendar.cpp
+++ Src/FrmCalendar.cpp
@@ -12,10 +12,10 @@ CFrmCalendar::CFrmCalendar(QWidget *parent) :
     CLunarCalendar::InitResource();
     m_pCalendar = new CLunarCalendar(this);
     m_pCalendar->SetShowWeeks(false);
-    //m_pCalendar->SetShowTools(false);
-    m_pCalendar->SetViewType(CLunarCalendar::ViewTypeWeek);
+    m_pCalendar->SetShowHead(false);
+    //m_pCalendar->SetViewType(CLunarCalendar::ViewTypeWeek);
     m_pCalendar->show();
-    m_pCalendar->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Preferred);
+    m_pCalendar->setSizePolicy(QSizePolicy::Minimum, QSizePolicy::Minimum);
     bool check = connect(m_pCalendar, SIGNAL(sigSelectionChanged()),
                          this, SLOT(slotSelectionChanged()));
     Q_ASSERT(check);
