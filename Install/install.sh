fba5878e854beb2537f8d2482d11c264abffc7e4
KangLin <kl222@126.com>
2019-06-04 07:25:57 +0800

    Modify install.sh

--- Install/install.sh
+++ Install/install.sh
@@ -13,8 +13,8 @@ case "$1" in
     install|*)
         echo "install ..."
         if [ ! -f /usr/share/applications/Tasks.desktop ]; then
-            ln -s `pwd`/Tasks/share/applications/Tasks.desktop /usr/share/applications/Tasks.desktop
-            ln -s `pwd`/Tasks/share/applications/Tasks.desktop /etc/xdg/autostart/Tasks.desktop
+            ln -s `pwd`/share/applications/Tasks.desktop /usr/share/applications/Tasks.desktop
+            ln -s `pwd`/share/applications/Tasks.desktop /etc/xdg/autostart/Tasks.desktop
             sed -i "s/Exec=.*//g" /usr/share/applications/Tasks.desktop
             echo "Exec=`pwd`/Tasks-x86_64.AppImage" >> /usr/share/applications/Tasks.desktop
         fi
@@ -22,7 +22,7 @@ case "$1" in
             if [ ! -d /usr/share/pixmaps ]; then
                 mkdir -p /usr/share/pixmaps
             fi
-            ln -s `pwd`/Tasks/share/pixmaps/tasks.png /usr/share/pixmaps/tasks.png
+            ln -s `pwd`/share/pixmaps/tasks.png /usr/share/pixmaps/tasks.png
         fi
         ;;
         
