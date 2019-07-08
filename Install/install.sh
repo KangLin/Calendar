e0fa7d9e5da755c553914f061f5036001d497575
KangLin <kl222@126.com>
2019-07-08 08:59:17 +0800

    Unix: Modify startup

--- Install/install.sh
+++ Install/install.sh
@@ -6,7 +6,7 @@ case "$1" in
     remove)
         echo "remove ..."
         rm -f /usr/share/applications/Tasks.desktop
-        rm -f /etc/xdg/autostart/Tasks.desktop
+        rm -f ~/.config/autostart/Tasks.desktop
         rm -f /usr/share/pixmaps/Tasks.png
     ;;
 
@@ -17,10 +17,10 @@ case "$1" in
         fi
         ln -s `pwd`/share/applications/Tasks.desktop /usr/share/applications/Tasks.desktop
 
-        if [ -f /etc/xdg/autostart/Tasks.desktop ]; then
-            rm /etc/xdg/autostart/Tasks.desktop
+        if [ -f ~/.config/autostart/Tasks.desktop ]; then
+            rm ~/.config/autostart/Tasks.desktop
         fi
-        ln -s `pwd`/share/applications/Tasks.desktop /etc/xdg/autostart/Tasks.desktop
+        ln -s `pwd`/share/applications/Tasks.desktop ~/.config/autostart/Tasks.desktop
 
         sed -i "s/Exec=.*//g" `pwd`/share/applications/Tasks.desktop
         echo "Exec=`pwd`/Tasks-x86_64.AppImage" >> `pwd`/share/applications/Tasks.desktop
