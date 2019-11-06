dcf7c5b58b3129a88f983fc4f053ab344b4b5194
KangLin <kl222@126.com>
2019-11-06 15:30:25 +0800

    add QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);

--- App/main.cpp
+++ App/main.cpp
@@ -15,6 +15,9 @@
 
 int main(int argc, char *argv[])
 {
+#if (QT_VERSION > QT_VERSION_CHECK(5,6,0))
+    QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+#endif
     QApplication a(argc, argv);
     a.setApplicationVersion(BUILD_VERSION);
     a.setApplicationName("Tasks");
