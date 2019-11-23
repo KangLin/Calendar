021233b34ee3fdc08b421eb364795cdffa70b70e
KangLin <kl222@126.com>
2019-11-22 19:35:25 -0800

    Release v0.2.9

--- ci/build.sh
+++ ci/build.sh
@@ -98,7 +98,7 @@ case ${BUILD_TARGERT} in
         ;;
 esac
 
-export VERSION="v0.2.8"
+export VERSION="v0.2.9"
 if [ "${BUILD_TARGERT}" = "unix" ]; then
     cd $SOURCE_DIR
     if [ "${DOWNLOAD_QT}" != "TRUE" ]; then
